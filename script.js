0f2edba89d5848f2386ad08a5deec60d03bca335
Vaceslav Ignatuk <vaceslavignatuk@gmail.com>
2021-10-31 21:36:24 +0300

    script fix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calc(operation, num1, num2) {
 	num1 = prompt('Введите первое значение');
 	operation = prompt('Выберите действие: сложение (+), вычитание (-), умножение(*), деление(/), возведение в степень(**)');
 	num2 = prompt('Введите второе значение');
-	if (typeof num1, num2 != 'number' || operation == '') {
+	if (typeof num1, num2 != 'number' || operation == '' || operation == 'string') {
 		alert('error');
 	} else if (operation == '+') {
 		return alert(+num1 + +num2);
