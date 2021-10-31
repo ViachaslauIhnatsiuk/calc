f2040ea25e3b25b6bd09d4b7e3348485f35a881e
Vaceslav Ignatuk <vaceslavignatuk@gmail.com>
2021-11-01 00:32:40 +0300

    refactoring fix

--- script.js
+++ script.js
@@ -4,24 +4,23 @@ function calc(num1, operation, num2) {
 	num1 = +prompt('Введите первое значение');
 	operation = prompt('Выберите действие: сложение (+), вычитание (-), умножение(*), деление(/), возведение в степень(**)');
 	num2 = +prompt('Введите второе значение');
-	if ((typeof num1 != 'number') || (typeof num2 != 'number') || operation == '' || operation == 'string') {
+	const isNotValid = (typeof num1 !== 'number') || (typeof num2 !== 'number') || operation === '' || operation === 'string';
+	if (isNotValid) {
 		alert('error');
-	} else if (operation == '+') {
+	} else if (operation === '+') {
 		return alert(num1 + num2);
-	} else if (operation == '-') {
+	} else if (operation === '-') {
 		return alert(num1 - num2);
-	} else if (operation == '*') {
+	} else if (operation === '*') {
 		return alert(num1 * num2);
-	} else if (operation == '/') {
+	} else if (operation === '/') {
 		return alert(num1 / num2);
-	} else if (operation == '**') {
+	} else if (operation === '**') {
 		return alert(num1 ** num2);
 	}
 };
 
 const start = document.getElementById('start');
 
-start.addEventListener('click', function () {
-	calc();
-})
+start.addEventListener('click', calc);
 
