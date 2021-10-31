6fd150b2cc5a9ae07225f838e656db32a9735ce2
Vaceslav Ignatuk <vaceslavignatuk@gmail.com>
2021-10-31 21:56:46 +0300

    last script fix

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 'use strict';
 
 function calc(num1, operation, num2) {
-	num1 = prompt('Введите первое значение');
+	num1 = +prompt('Введите первое значение');
 	operation = prompt('Выберите действие: сложение (+), вычитание (-), умножение(*), деление(/), возведение в степень(**)');
-	num2 = prompt('Введите второе значение');
-	if (typeof num1, num2 != 'number' || operation, num1, num2 == '') {
+	num2 = +prompt('Введите второе значение');
+	if ((typeof num1 != 'number') || (typeof num2 != 'number') || operation == '' || operation == 'string') {
 		alert('error');
 	} else if (operation == '+') {
-		return alert(+num1 + +num2);
+		return alert(num1 + num2);
 	} else if (operation == '-') {
 		return alert(num1 - num2);
 	} else if (operation == '*') {
@@ -24,3 +24,4 @@ const start = document.getElementById('start');
 start.addEventListener('click', function () {
 	calc();
 })
+
