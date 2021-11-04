a5f0e68aa4b6e092a8da6b0d0df174dfd47aa1bb
Vaceslav Ignatuk <vaceslavignatuk@gmail.com>
2021-11-04 22:26:38 +0300

    calc modified with objects

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// 'Калькулятор на prompt и alert'
+
 /* function calc(num1, operation, num2) {
 	num1 = +prompt('Введите первое значение');
 	operation = prompt('Выберите действие: сложение (+), вычитание (-), умножение(*), деление(/), возведение в степень(**)');
@@ -25,14 +27,15 @@ const start = document.getElementById('start');
 start.addEventListener('click', calc); */
 
 
+// 'Калькулятор для консоли'
 
 
-function calc(num1, operation, num2) {
+/* function calc(num1, operation, num2) {
 	const isNotValid = (typeof num1 !== 'number') || (typeof num2 !== 'number') || operation === '';
 	if (isNotValid) {
 		return 'error';
 	} else if (operation === '+') {
-		return +num1 + +num2;
+		return num1 + num2;
 	} else if (operation === '-') {
 		return num1 - num2;
 	} else if (operation === '*') {
@@ -48,4 +51,53 @@ function calc(num1, operation, num2) {
 	}
 };
 
-console.log(calc(5, '-', 4));
+console.log(calc(5, '+', 6)); */
+
+
+// 'Калькулятор на switch case'
+
+/* function calc(num1, operation, num2) {
+	const isNotValid = (typeof num1 !== 'number') || (typeof num2 !== 'number') || operation === '';
+	if (isNotValid) return 'error';
+	switch (operation) {
+		case '+':
+			return num1 + num2;
+		case '-':
+			return num1 - num2;
+		case '*':
+			return num1 * num2;
+		case '/':
+			return num1 / num2;
+		case '**':
+			return num1 ** num2;
+		case '%':
+			return num1 % num2;
+		default:
+			return 'unknown operation';
+	}
+};
+
+console.log(calc(6, '**', 5)); */
+
+
+
+// 'Калькулятор на objects'
+
+function calc(num1, operation, num2) {
+	const isNotValid = (typeof num1 !== 'number') || (typeof num2 !== 'number') || operation === '';
+	let operations = {
+		sum: num1 + num2,
+		sub: num1 - num2,
+		mult: num1 * num2,
+		div: num1 / num2,
+	}
+	if (isNotValid) {
+		return 'error';
+	} else if (operation !== undefined) {
+		return operations[operation];
+	} else {
+		return 'unknown operation';
+	}
+};
+
+console.log(calc(6, 'div', 5));
